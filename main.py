f95ce971a544689ea25dc9e30e4645125d9b949c
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-10-08 18:02:33 -0600

    Esto

--- main.py
+++ main.py
@@ -10,24 +10,9 @@ r = Render(800,800)
 r.glCreateWindow(800,800)
 r.glViewPort(0,0,999,999)
 
-t = Texture('model.bmp')
+t = Texture('coca.bmp')
 
 # Rotacion a la Derecha
 r.lookAt(V3(-0.2,0,20), V3(0,0,0), V3(0,1,0))
-r.load('model.obj',translate=(0,0,0), scale=(0.5,0.5,0.5), rotate=(0,0.2,0.36), texture=t)
-r.archivo('Dutch.bmp')
-
-# Rotacion Izquierda
-#r.lookAt(V3(1,0,5), V3(0,0,0), V3(0,1,0))
-#r.load('model.obj',translate=(0,0,0), scale=(0.5,0.5,0.5), rotate=(0,-1,0), texture=t)
-#r.archivo('Izquierda.bmp')
-
-# Arriba
-#r.lookAt(V3(1,0,5), V3(0,0,0), V3(0,1,0))
-#r.load('model.obj',translate=(0,0,0), scale=(0.5,0.5,0.5), rotate=(1,0,0), texture=t)
-#r.archivo('Arriba.bmp')
-
-# Low
-#r.lookAt(V3(1,0,5), V3(0,0,0), V3(0,1,0))
-#r.load('model.obj',translate=(0,0,0), scale=(0.5,0.5,0.5), rotate=(-0.5,0,0), texture=t)
-#r.archivo('Low.bmp')
+r.load('cocacola.obj',translate=(0,0,0), scale=(0.5,0.5,0.5), rotate=(0,0.2,0.36), texture=t)
+r.archivo('Coquita.bmp')
